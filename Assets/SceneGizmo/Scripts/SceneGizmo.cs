725fb50478c4557af78c4fa667bb84885a6f5447
nikita <koz.nikita@gmail.com>
2018-03-12 20:28:37 +0300

    SceneGizmo position fix

--- Assets/SceneGizmo/Scripts/SceneGizmo.cs
+++ Assets/SceneGizmo/Scripts/SceneGizmo.cs
@@ -27,11 +27,23 @@ namespace EMSP
         #endregion
 
         #region Fields
-        public Vector3 ViewportOffset = new Vector3(0.8f, 0.8f, 0.1f);
+        [Range(0,10)]
+        public float Offset = 0;
+
+        [SerializeField]
+        private Transform _topPoint;
+        [SerializeField]
+        private Transform _bottomPoint;
+
         private Camera _gizmoCamera;
         private Transform _transform;
 
+        private Vector3 _baseOffset;
+        private float _tenthPartOfGizmoSize;
 
+        private bool _isInitialized = false;
+        private float _currentScreenWidth;
+        private float _currentScreenHeight;
         #endregion
 
         #region Events
@@ -50,19 +62,50 @@ namespace EMSP
 
         private void Start()
         {
-            _transform = transform;
-            _gizmoCamera = GameObject.FindGameObjectWithTag("GizmoCamera").GetComponent<Camera>();
-
-            foreach(var handler in GetComponentsInChildren<GizmoDirectionsClickHandler>())
+            if (!_isInitialized)
             {
-                handler.ClickEvent += OnGizmoDirectionClick;
+                _transform = transform;
+                _gizmoCamera = GameObject.FindGameObjectWithTag("GizmoCamera").GetComponent<Camera>();
+
+                foreach (var handler in GetComponentsInChildren<GizmoDirectionsClickHandler>())
+                {
+                    handler.ClickEvent += OnGizmoDirectionClick;
+                }
+
+                _isInitialized = true;
             }
+
+            _currentScreenWidth = Screen.width;
+            _currentScreenHeight = Screen.height;
+
+            var rawHeight = Vector3.Magnitude(_topPoint.position - _bottomPoint.position);
+            _transform.position = _gizmoCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, rawHeight));
+            _transform.rotation = _gizmoCamera.transform.rotation;
+
+            var scrTop = _gizmoCamera.WorldToScreenPoint(_topPoint.position);
+            var scrBottom = _gizmoCamera.WorldToScreenPoint(_bottomPoint.position);
+            var gizmoHeight = Vector3.Magnitude(scrTop - scrBottom);
+
+            _tenthPartOfGizmoSize = gizmoHeight / 10;
+            _baseOffset = new Vector3(gizmoHeight / 2, gizmoHeight / 2, rawHeight);
         }
 
         private void LateUpdate()
         {
-            _transform.position = _gizmoCamera.ViewportToWorldPoint(new Vector3(ViewportOffset.x, ViewportOffset.y, Camera.main.nearClipPlane + ViewportOffset.z));
-            _transform.rotation = Quaternion.identity;
+            var newScreenPosition = new Vector3(
+                Screen.width - (_baseOffset.x + Offset * _tenthPartOfGizmoSize), 
+                Screen.height - 20 - (_baseOffset.y + Offset * _tenthPartOfGizmoSize), // 20 - menu panel height
+                _baseOffset.z
+                );
+
+            _transform.position = _gizmoCamera.ScreenToWorldPoint(newScreenPosition);
+            _transform.rotation = Quaternion.identity;        
+        }
+
+        private void Update()
+        {
+            if (_currentScreenWidth != Screen.width || _currentScreenHeight != Screen.height)
+                Start();
         }
 
         #endregion
