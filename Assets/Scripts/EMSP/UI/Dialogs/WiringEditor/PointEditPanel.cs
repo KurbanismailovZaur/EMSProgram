a1ee3fab57d43a7aa347cf1430df729423a9947b
NAKozyrev <koz.nikita@gmail.com>
2018-03-17 02:45:24 +0300

    part of behaviour added

---
+++ Assets/Scripts/EMSP/UI/Dialogs/WiringEditor/PointEditPanel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EMSP.UI.Dialogs.WiringEditor
+{
+    public class PointEditPanel : MonoBehaviour
+    {
+        #region Entities
+        #region Enums
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Structures
+        #endregion
+
+        #region Classes
+        #endregion
+
+        #region Interfaces
+        #endregion
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Behaviour
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+
+        public void Initialize(int pointNumber, Vector3 point)
+        {
+        }
+        #endregion
+
+        #region Indexers
+        #endregion
+
+        #region Events handlers
+        #endregion
+        #endregion
+    }
+}
--- Assets/Scripts/EMSP/UI/Dialogs/WiringEditor/WiringEditorDialog.cs
+++ Assets/Scripts/EMSP/UI/Dialogs/WiringEditor/WiringEditorDialog.cs
@@ -1,16 +1,119 @@
-﻿using System.Collections;
+﻿using EMSP.Communication;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class WiringEditorDialog : MonoBehaviour {
+namespace EMSP.UI.Dialogs.WiringEditor
+{
+    public class WiringEditorDialog : MonoBehaviour
+    {
+        #region Entities
+        #region Enums
+        #endregion
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        #region Delegates
+        #endregion
+
+        #region Structures
+        #endregion
+
+        #region Classes
+        #endregion
+
+        #region Interfaces
+        #endregion
+        #endregion
+
+        #region Fields
+        public Button WireButtonPrefab;
+        public PointEditPanel PointEditPanelPrefab;
+
+        public RectTransform WireButtonContainer;
+        public RectTransform PointsContainer;
+
+        private Dictionary<int, List<Vector3>> _wiring = new Dictionary<int, List<Vector3>>();
+        private CanvasGroup _canvasGroup;
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Behaviour
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public void StartWiringEditing(Wiring wiring) 
+        {
+            _canvasGroup.alpha = 1;
+
+            int wireCount = 0;
+            foreach(Wire wire in wiring)
+            {
+                Button wireButton = Instantiate(WireButtonPrefab);
+                wireButton.transform.parent = WireButtonContainer;
+                wireButton.GetComponentInChildren<Text>().text = string.Format("Wire_{0}", wireCount);
+
+                wireButton.onClick.AddListener(() =>
+                {
+                    for(int i = 0; i < PointsContainer.childCount; ++i)
+                    {
+                        Destroy(PointsContainer.GetChild(i).gameObject);
+                    }
+
+                    int pointCount = 0;
+                    foreach (Vector3 point in wire)
+                    {
+                        PointEditPanel editPanel = Instantiate(PointEditPanelPrefab);
+                        editPanel.transform.parent = PointsContainer;
+
+                        editPanel.Initialize(pointCount, point);
+                        ++pointCount;
+                    }
+                });
+
+                ++wireCount;
+            }
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Close()
+        {
+            for (int i = 0; i < WireButtonContainer.childCount; ++i)
+            {
+                Destroy(WireButtonContainer.GetChild(i).gameObject);
+            }
+
+            _canvasGroup.alpha = 0;
+        }
+
+        #endregion
+
+        #region Indexers
+        #endregion
+
+        #region Events handlers
+        #endregion
+        #endregion
+
+        public void Test()
+        {
+            Wiring.Factory factory = new Wiring.Factory();
+            
+        }
+    }
 }
