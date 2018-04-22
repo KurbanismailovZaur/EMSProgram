4e697765d0e099902df6e30ae7732aec9f2e2de8
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-04-23 01:50:17 +0300

    Added CalculationSettingsDialog

---
+++ Assets/Scripts/EMSP/UI/Dialogs/CalculationSettings/CalculationSettingsDialog.cs
@@ -0,0 +1,53 @@
+﻿using EMSP.Mathematic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EMSP.UI.Dialogs.CalculationSettings
+{
+	public class CalculationSettingsDialog : MonoBehaviour 
+	{
+        #region Entities
+        #region Enums
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Structures
+        #endregion
+
+        #region Classes
+        #endregion
+
+        #region Interfaces
+        #endregion
+        #endregion
+
+        #region Fields
+        [SerializeField]
+        private InputField _inputField;
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Behaviour
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Indexers
+        #endregion
+
+        #region Events handlers
+        #endregion
+        #endregion
+    }
+}
