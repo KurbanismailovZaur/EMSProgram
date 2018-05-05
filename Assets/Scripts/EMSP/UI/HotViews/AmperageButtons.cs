19495d95a24b60f874727d3e37aee9d79bfbc059
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-05-05 21:40:47 +0300

    Added hot buttons for switch amperage mode

---
+++ Assets/Scripts/EMSP/UI/HotViews/AmperageButtons.cs
@@ -0,0 +1,75 @@
+﻿using EMSP.Mathematic;
+using EMSP.UI.Toggle;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EMSP.UI.HotViews
+{
+	public class AmperageButtons : MonoBehaviour 
+	{
+        #region Entities
+        #region Enums
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Structures
+        #endregion
+
+        #region Classes
+        #endregion
+
+        #region Interfaces
+        #endregion
+        #endregion
+
+        #region Fields
+        [SerializeField]
+        private ToggleGroup _toggleGroup;
+
+        [SerializeField]
+        private ToggleBehaviour _staticToggleBehaviour;
+
+        [SerializeField]
+        private ToggleBehaviour _dynamicToggleBehaviour;
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Behaviour
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Indexers
+        #endregion
+
+        #region Events handlers
+        public void ToggleGroup_TryToToggleBehaviourSelecting(ToggleGroup toggleGroup, ToggleBehaviour toggleBehaviour)
+        {
+            if (toggleBehaviour == _staticToggleBehaviour)
+            {
+                MathematicManager.Instance.AmperageMode = AmperageMode.Precomputed;
+            }
+            else
+            {
+                MathematicManager.Instance.AmperageMode = AmperageMode.Computational;
+            }
+        }
+
+        public void MathematicManager_AmperageModeChanged(MathematicManager mathematicManager, AmperageMode amperageMode)
+        {
+            _toggleGroup.SelectToggleBehaviour(amperageMode == AmperageMode.Computational ? _dynamicToggleBehaviour : _staticToggleBehaviour);
+        }
+        #endregion
+        #endregion
+    }
+}
